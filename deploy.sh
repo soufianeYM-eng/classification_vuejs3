1bb1db7ab4e9bb5b3d51e698be68681268861805
Soufiane Yousfi <yousfisoufiane8@gmail.com>
2021-03-20 21:35:25 +0100

    last

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/soufianeYM-eng/classification_vuejs.git
+git push -f https://github.com/soufianeYM-eng/classification_vuejs3.git
 
 cd -
